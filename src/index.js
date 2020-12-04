66b6eb7b270497e090481097efd4c72000134503
summer <1249130528@qq.com>
2020-12-04 23:59:29 +0800

    测试删除

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ class Square extends React.Component {
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
-            {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
           </div>
