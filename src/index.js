07ae9e1a338f43ba743ec1ec4b8c62efdec821f6
summer <1249130528@qq.com>
2020-12-05 00:29:05 +0800

    Revert "测试删除"
    This reverts commit 66b6eb7b270497e090481097efd4c72000134503.

--- src/index.js
+++ src/index.js
@@ -23,10 +23,12 @@ class Square extends React.Component {
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
+            {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
           </div>
           <div className="board-row">
+            {this.renderSquare(3)}
             {this.renderSquare(4)}
             {this.renderSquare(5)}
           </div>
